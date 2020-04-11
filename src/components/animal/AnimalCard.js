07ed6755832b0ab4acdd32d9cc54eb72485e9dd8
Luma <luma@icarustech.com.br>
2020-04-11 20:29:44 -0300

    Criado componente e passadas props ao componente AnimalCard.
    Funcionando para deletar o animal.

---
+++ src/components/animal/AnimalCard.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Alert } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+class AnimalCard extends Component {
+  
+  render() {
+    const { animal, createAlertDelete } = this.props;
+
+    return (
+      <View style={styles.card}>
+        <View style={styles.container}>
+          <View style={styles.containerAnimal}>
+            <View>
+              <Text style={styles.nomeAnimal}>{animal.nome}</Text>
+            </View>
+            <View style={styles.containerInfo}>
+              <View>
+                <View>
+                  <Text style={styles.subtitulo}>Tipo de Animal</Text>
+                </View>
+                <View style={styles.tipoAnimalContainer}>
+                  <View style={styles.tipoAnimalBadge}>
+                    <Text style={styles.tipoAnimal}>{animal.tipo}</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={styles.containerPeso}>
+                <View>
+                  <Text style={styles.subtitulo}>Peso</Text>
+                </View>
+                <View style={styles.pesoAnimal}>
+                  <Text style={styles.pesoAnimal}>344,5 kg</Text>
+                </View>
+              </View>
+            </View>
+            <View>
+              <View style={styles.localAnimalContainer}>
+                <Icon style={styles.iconeLocal} name="md-home" size={25} />
+                <Text style={styles.localAnimal}>Galpão 1</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.containerBotoes}>
+            <Icon style={styles.icon} name="md-eye" />
+            <Icon style={styles.icon} name="md-trash" onPress={() => {createAlertDelete(animal.id)}} />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    backgroundColor: '#f8f8ff',
+    marginTop: 8,
+    marginHorizontal: 16,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: '#daeff8',
+    borderRadius: 8,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  containerAnimal: {
+    flex: 0.93,
+  },
+  containerBotoes: {
+    flex: 0.07,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: 30,
+  },
+  nomeAnimal: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  containerInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 6,
+  },
+  containerPeso: {
+    marginRight: 40,
+    marginLeft: 70,
+    paddingLeft: 15,
+    borderLeftWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#dcdcdc',
+  },
+  subtitulo: {
+    textTransform: 'uppercase',
+    fontSize: 10,
+    fontWeight: 'bold',
+    color: '#808080',
+  },
+  tipoAnimalContainer: {
+    marginTop: 5,
+  },
+  tipoAnimalBadge: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 65,
+    height: 22,
+    borderRadius: 12,
+    backgroundColor: '#0047ab',
+  },
+  tipoAnimal: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 12,
+    textTransform: 'capitalize',
+  },
+  pesoAnimal: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  localAnimalContainer: {
+    flexDirection: 'row',
+    marginTop: 8,
+  },
+  iconeLocal: {
+    fontSize: 22,
+    color: '#0047ab',
+  },
+  localAnimal: {
+    marginTop: 3,
+    marginLeft: 6,
+    color: '#696969',
+  },
+  icon: {
+    fontSize: 22,
+  },
+});
+
+export default AnimalCard;
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { SafeAreaView, ScrollView, StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View, Text, Alert } from 'react-native';
 import { API_URL } from '../../app.consts';
-
-import Icon from 'react-native-vector-icons/Ionicons';
+import AnimalCard from './AnimalCard';
 
 class AnimalList extends Component {
   state = {
@@ -17,6 +16,31 @@ class AnimalList extends Component {
     });
   }
 
+  delete(animalId) {
+    axios.delete(`${API_URL}/animal/${animalId}`).then((res) => {
+      const { animals } = this.state;
+      const animalsList = animals.filter(res => res.id !== animalId);
+
+      this.setState({ animals: animalsList });
+    });
+  }
+
+  createAlertDelete(animalId) {
+    Alert.alert(
+      'Deseja realmente excluir o animal?',
+      '',
+      [
+        {
+          text: 'Cancelar',
+          onPress: () => {},
+          style: 'cancel',
+        },
+        {text: 'Ok', onPress: () => this.delete(animalId)},
+      ],
+      {cancelable: false},
+    );
+  }
+
   render() {
     const { animals } = this.state;
     let cards = null;
@@ -24,37 +48,11 @@ class AnimalList extends Component {
     if (animals.length > 0) {
       cards = animals.map((item, index) => {
         return (
-          <View style={styles.card}>
-            <View>
-              <Text style={styles.nomeAnimal}>{item.nome}</Text>
-            </View>
-            <View style={styles.containerInfo}>
-              <View>
-                <View>
-                  <Text style={styles.subtitulo}>Tipo de Animal</Text>
-                </View>
-                <View style={styles.tipoAnimalContainer}>
-                  <View style={styles.tipoAnimalBadge}>
-                    <Text style={styles.tipoAnimal}>{item.tipo}</Text>
-                  </View>
-                </View>
-              </View>
-              <View style={styles.containerPeso}>
-                <View>
-                  <Text style={styles.subtitulo}>Peso</Text>
-                </View>
-                <View style={styles.pesoAnimal}>
-                  <Text style={styles.pesoAnimal}>344,5 kg</Text>
-                </View>
-              </View>
-            </View>
-            <View>
-              <View style={styles.localAnimalContainer}>
-                <Icon style={styles.iconeLocal} name="md-home" size={25} />
-                <Text style={styles.localAnimal}>Galpão 1</Text>
-              </View>
-            </View>
-          </View>
+          <AnimalCard 
+            key={item.id} 
+            animal={item}
+            createAlertDelete={this.createAlertDelete.bind(this)}>
+          </AnimalCard>
         );
       });
     }
@@ -78,76 +76,7 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     fontSize: 25,
     color: '#1c1c1c',
-  },
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#f8f8ff',
-  },
-  card: {
-    marginTop: 8,
-    marginHorizontal: 16,
-    padding: 10,
-    borderWidth: 2,
-    borderColor: '#daeff8',
-    borderRadius: 8,
-  },
-  nomeAnimal: {
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  containerInfo: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 6,
-  },
-  containerPeso: {
-    marginRight: 75,
-    paddingLeft: 15,
-    borderLeftWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#dcdcdc',
-  },
-  subtitulo: {
-    textTransform: 'uppercase',
-    fontSize: 10,
-    fontWeight: 'bold',
-    color: '#808080',
-  },
-  tipoAnimalContainer: {
-    marginTop: 5,
-  },
-  tipoAnimalBadge: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 65,
-    height: 22,
-    borderRadius: 12,
-    backgroundColor: '#0047ab',
-  },
-  tipoAnimal: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 12,
-    textTransform: 'capitalize',
-  },
-  pesoAnimal: {
-    fontWeight: 'bold',
-    fontSize: 18,
-  },
-  localAnimalContainer: {
-    flexDirection: 'row',
-    marginTop: 8,
-  },
-  iconeLocal: {
-    fontSize: 22,
-    color: '#0047ab',
-  },
-  localAnimal: {
-    marginTop: 3,
-    marginLeft: 6,
-    color: '#696969',
-  },
+  }
 });
 
 export default AnimalList;
